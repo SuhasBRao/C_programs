7b98a96ae3c4dc219c6254c154629adbf0274548
SuhasBRao <raos04567@gmail.com>
2021-04-22 11:53:12 +0530

    Updated Doubly linked list

--- Doubly_linked_list.c
+++ Doubly_linked_list.c
@@ -46,19 +46,48 @@ struct node *add_at_end(struct node *head)
     
     return head;
 }
-int main()
+
+struct node *add_at_beg(struct node *head)
 {
-   struct node *head = malloc(sizeof(struct node));
+    int data;
+    struct node *temp = malloc(sizeof(struct node));
+    printf("Enter data:");
+    scanf("%d",&data);
+    temp->prev = NULL;
+    temp->data = data;
+    temp->next = head;
+    head->prev = temp;
+
+    head = temp;
+    return head;
+}
+
+int display(struct node *head)
+{  
    struct node *ptr;
-   head = create_doubly_ll(head);
-   printf("Created a doubly linked list\n");
-   head = add_at_end(head);
    ptr = head;
-   printf("Updated Doubly linked list is\n");
    while(ptr != NULL)
    {
        printf("%d->",ptr->data);
        ptr = ptr->next;
    }
+    printf("\n");
+    return 0;
 
+}
+int main()
+{
+   struct node *head = malloc(sizeof(struct node));
+   
+   head = create_doubly_ll(head);
+   printf("Created a doubly linked list\n");
+   display(head);
+   head = add_at_end(head);
+   printf("Added at end\n");
+   display(head);
+   head = add_at_beg(head);
+   printf("Added at beginning\n");
+   display(head);
+
+   return 0;
 }

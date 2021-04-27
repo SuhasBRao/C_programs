7e961bcd6d3c49192218b69219958d94a7b192d5
Suhas B Rao <raos04567@gmail.com>
2021-04-27 19:52:13 +0530

    Updated doubly_linked_list.c

--- Doubly_linked_list.c
+++ Doubly_linked_list.c
@@ -22,6 +22,7 @@ struct node *create_doubly_ll(struct node *head)
 
     return head;
 }
+
 struct node *add_at_end(struct node *head)
 {
     struct node *temp = malloc(sizeof(struct node));
@@ -38,12 +39,12 @@ struct node *add_at_end(struct node *head)
         //printf("%d->",ptr->data);
         ptr = ptr->next;
     }
-    
+
     temp->prev = ptr;
     temp->next = NULL;
     ptr->next = temp;
     head = ptr;
-    
+
     return head;
 }
 
@@ -63,7 +64,7 @@ struct node *add_at_beg(struct node *head)
 }
 
 int display(struct node *head)
-{  
+{
     struct node *ptr;
     ptr = head;
     while(ptr != NULL)
@@ -78,16 +79,37 @@ int display(struct node *head)
 int main()
 {
    struct node *head = malloc(sizeof(struct node));
-   
-   head = create_doubly_ll(head);
-   printf("Created a doubly linked list\n");
-   display(head);
-   head = add_at_end(head);
-   printf("Added at end\n");
-   display(head);
-   head = add_at_beg(head);
-   printf("Added at beginning\n");
-   display(head);
-   
+   int val;
+   again:
+     printf("###############################\n");
+     printf("Please choose an operation:\n");
+     printf("[1]Create a single node for doubly linked list\n[2]Add node at end\n");
+     printf("[3]Add node at beginning\n[4]Display doubly linked list\n[5]Stop the operations\n" );
+     scanf("%d",&val);
+     switch(val)
+     {
+       case 1:head = create_doubly_ll(head);
+              printf("Created a node for doubly linked list\n");
+              break;
+       case 2:head = add_at_end(head);
+              printf("Added at end\n");
+              break;
+       case 3:head = add_at_beg(head);
+              printf("Added at beginning\n");
+              break;
+       case 4:printf("Doubly linked list is as follows\n");
+              display(head);
+              break;
+       case 5:printf("Stopping the program\n");
+              break;
+
+       default: printf("Please choose the operation correctly\n");
+     }
+   if (val != 5)
+   {
+     goto again;
+   }
+
+
    return 0;
 }

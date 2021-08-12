012b9acb8fb4c589db69d9f9ac90a8dfed2e757d
SuhasBRao <raos04567@gmail.com>
2021-08-12 06:53:51 +0530

    added c puzzels

---
+++ C puzzels/print_numbers_to_n.c
@@ -0,0 +1,9 @@
+/*Program prints numbers upto n without using semicolon
+*/
+#include<stdio.h>
+#define NUM 10
+int main(int num){
+    if (num<=NUM && printf("%d ",num) && main(num+1)){
+
+    }
+}
